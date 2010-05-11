9fdf6e3105d3b492769b13b226211be7065134cd
Jeffrey Drake <iaefai@me.com>
2010-05-11 00:29:26 -0400

    Reorganized source tree, please report any troubles

---
+++ Source/ConsoleDocument.h
@@ -0,0 +1,16 @@
+//
+//  ConsoleDocument.h
+//  Hcode
+//
+//  Created by Jeffrey Drake on 10-05-11.
+//  Copyright 2010 Jeffrey Drake. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ConsoleDocument : NSDocument {
+
+}
+
+@end
